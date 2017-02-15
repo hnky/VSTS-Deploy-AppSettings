7a9fcc3164876c98281e9fd269907e4d6e114411
Henk Boelman <henk@boelman.eu>
2017-02-16 00:02:41 +0100

    First version

---
+++ AzureWebappSetAppSettings/sample.ps1
@@ -0,0 +1,53 @@
+[CmdletBinding()]
+param(
+    [String] [Parameter(Mandatory = $true)]
+    $ConnectedServiceName,
+
+    [String] [Parameter(Mandatory = $true)]
+    $WebAppName,
+
+    [String] [Parameter(Mandatory = $true)]
+    $ResourceGroupName,
+
+    [String] [Parameter(Mandatory = $true)]
+    $AppSettings
+)
+
+# For more information on the VSTS Task SDK:
+# https://github.com/Microsoft/vsts-task-lib
+
+
+import-module "Microsoft.TeamFoundation.DistributedTask.Task.Internal"
+import-module "Microsoft.TeamFoundation.DistributedTask.Task.Common"
+
+ 
+    Write-Host ($WebAppName)
+    Write-Host ($ResourceGroupName)
+    Write-Host ($AppSettings)
+
+
+
+$lines = $AppSettings.Replace("`r","").Split()
+Write-Host ("Lines found: " + $lines.Count)
+
+
+$activeAppSettings = Get-AzureRMWebApp -ResourceGroupName $ResourceGroupName -Name $WebAppName 
+$appSettingList = $activeAppSettings.SiteConfig.AppSettings
+
+$hash = @{}
+
+foreach ($kvp in $appSettingList) {
+    $hash[$kvp.Name] = $kvp.Value
+    Write-Host ("Found - Key: " + $kvp.Name + " Value: " + $kvp.Value)
+}
+
+foreach ($keyValue in $lines) {
+	$key,$val = $keyValue.Split("=")
+    $hash[$key.Trim()] = $val.Trim()
+     Write-Host ("Adding - Key: " + $key + " Value: " + $val)
+}
+
+Set-AzureRMWebApp -ResourceGroupName $ResourceGroupName -Name $WebAppName -AppSettings $hash
+Write-Host("Done!!")
+    # Output the message to the log.
+
